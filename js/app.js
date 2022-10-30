f96e9aab125f7289861cd336ab90e344cb8fc290
AbdulMalik Sharif <abdumaliksharipov@gmail.com>
2022-10-31 00:16:17 +0500

    footer&&backtotop

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventOnElem(type, callback);
+      elem[i].addEventListener(type, callback);
     }
   } else {
     elem.addEventListener(type, callback);
@@ -13,10 +13,12 @@ const addEventOnElem = function (elem, type, callback) {
 
 // defining vars
 const navbar = document.querySelector('.navbar');
-const navTogglers = document.querySelectorAll('[data-nav-toggler]');
+const navTogglers = document.querySelectorAll('nav-close-btn');
 const navLinks = document.querySelectorAll('.navbar-item');
 const overlay = document.querySelector('.overlay');
-const header = docuement.querySelector('.header');
+const header = document.querySelector('.header');
+const backTopBtn = document.querySelector('.back-top-btn');
+const year = document.querySelector('.year');
 
 // defininig callbacks
 const toggleNav = function () {
@@ -37,9 +39,13 @@ addEventOnElem(navLinks, 'click', closeNavbar);
 const activeElem = function () {
   if (window.scrollY > 100) {
     header.classList.add('active');
+    backTopBtn.classList.add('active');
   } else {
     header.classList.remove('active');
+    backTopBtn.classList.remove('active');
   }
 };
 
 addEventOnElem(window, 'scroll', activeElem);
+
+year.textContent = `${new Date().getFullYear()}.`;
